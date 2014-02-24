d3bdad759f4c35aea627b4ddb868431f16ce8f9b
Brian Adams <badams@gnarlie.com>
2014-02-24 15:45:28 -0600

    add files

---
+++ src/interrupt.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "common.h"
+
+#define IRQ0 32
+#define IRQ1 33
+
+typedef struct registers {
+    uint64_t ds;
+    uint64_t es;
+    uint64_t fs;
+    uint64_t gs;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rax;
+    uint64_t rbx;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t errorCode;
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+} registers_t;
+
+typedef void (*isr_t)(registers_t*);
+
+void init_interrupts();
+void register_interrupt_handler(uint8_t interrupt, isr_t);
---
+++ src/rtc.c
@@ -0,0 +1,66 @@
+#include "rtc.h"
+
+#include "common.h"
+
+#define CMOS_ADDR 0x70
+#define CMOS_DATA 0x71
+
+static int is_update_in_progress() {
+    outb(CMOS_ADDR, 0x0a);
+    return inb(0x71) & 0x80;
+}
+
+static char get_cmos_reg(int reg) {
+    outb(CMOS_ADDR, reg);
+    return inb(CMOS_DATA);
+}
+
+static uint32_t seconds_of(int year, int month, int day) {
+    return 0;
+}
+
+static uint8_t from_bcd(char x) {
+    return (x & 0xf) + (x / 16) * 10;
+}
+
+static uint32_t read_rtc_internal() {
+    while(is_update_in_progress())
+        asm volatile ("pause");
+
+    char second = get_cmos_reg(0x00);
+    char minute = get_cmos_reg(0x02);
+    char hour = get_cmos_reg(0x04);
+    char day = get_cmos_reg(0x07);
+    char month = get_cmos_reg(0x08);
+    uint16_t year = get_cmos_reg(0x09);
+    //todo century register ... ?
+
+    char regB = get_cmos_reg(0x0B);
+    if (0 == (regB & 0x04)) {
+        second = from_bcd(second);
+        minute = from_bcd(minute);
+        hour = from_bcd(hour);
+        day = from_bcd(day);
+        month = from_bcd(month);
+    }
+
+    if (0 == (regB & 0x02) && (hour & 0x80)) {
+        if (hour & 0x80) hour = 0;
+        else hour = (hour & 0x7f + 12 % 24);
+    }
+
+    year += 2000;
+
+    return seconds_of(year, month, day) +
+        ((hour * 60) + minute) * 60 + second;
+}
+
+uint32_t read_rtc() {
+    uint32_t a, b;
+    do {
+        a = read_rtc_internal();
+        b = read_rtc_internal();
+    } while (a != b);
+
+    return a;
+}
