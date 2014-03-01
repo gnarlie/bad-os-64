18a886fd5f5db27a81849dea7ef4142ad4ed05e9
Brian Adams <badams@gnarlie.com>
2014-03-01 17:04:28 -0600

    fix tests

--- test/alltests.c
+++ test/alltests.c
@@ -7,6 +7,9 @@ void panic(const char * why) {
 void disable_interrupts() {}
 void enable_interrupts() {}
 
+void register_interrupt_handler(int interrupt, void* handler) {
+}
+
 int main(int argc, char**argv) {
     int rc = 0;
     RUN(simpleAllocation); rc |= TEST_REPORT();
