971fac30d7ca378cda7587f8d6dc5241c3597674
Brian Adams <badams@gnarlie.com>
2014-06-17 07:53:16 -0500

    connection succeeded

--- src/net/ip.h
+++ src/net/ip.h
@@ -15,6 +15,7 @@ struct sbuff_t* ip_sbuff_alloc(uint16_t sz);
 static inline uint16_t checksum(const void *buf, uint32_t len, uint16_t* dest) {
     const uint8_t* data= (const char*)buf;
     uint32_t sum = 0;
+    *dest = 0;
 
     for (int i = 0; i < len; i+=2) {
         uint16_t w = ((data[i]<<8) & 0xff00) +
--- src/net/tcp.c
+++ src/net/tcp.c
@@ -3,6 +3,7 @@
 #include "net/sbuff.h"
 #include "net/ntox.h"
 #include "net/ip.h"
+#include "memory.h"
 
 typedef struct stream_t {
     uint16_t localPort;
@@ -15,6 +16,14 @@ typedef struct stream_t {
     uint8_t needsAck;
 } stream;
 
+typedef struct {
+    uint32_t src;
+    uint32_t dst;
+    uint8_t zero;
+    uint8_t proto;
+    uint16_t len;
+} tcp_pseudo_hdr;
+
 enum {
     Fin = 0x001,
     Syn = 0x002,
@@ -26,6 +35,18 @@ enum {
     Cwr = 0x080,
     Ns = 0x100
 };
+
+static void tcp_checksum(sbuff *sb, uint16_t len, uint32_t src, uint32_t dest) {
+    tcp_hdr * hdr = (tcp_hdr*) sb->head;
+
+    tcp_pseudo_hdr *pseudo = (tcp_pseudo_hdr*)(sb->head - sizeof(tcp_pseudo_hdr));
+    pseudo->src = ntol(src);
+    pseudo->dst = ntol(dest);
+    pseudo->zero = 0;
+    pseudo->proto = IPPROTO_TCP;
+    pseudo->len = ntos(len);
+    checksum(pseudo, sizeof(*pseudo) + len, &hdr->chksum);
+}
 
 void reset_stream(struct netdevice *dev, stream* stream) {
     sbuff * sb = ip_sbuff_alloc(sizeof(tcp_hdr));
@@ -40,9 +61,32 @@ void reset_stream(struct netdevice *dev, stream* stream) {
     hdr->flags = Rst | (stream->needsAck ? Ack : 0);
     hdr->window = 0;
 
-    checksum(hdr, sizeof(*hdr), &hdr->chksum);
+    tcp_checksum(sb, sizeof(*hdr), dev->ip, stream->remoteAddr);
 
     ip_send(sb, IPPROTO_TCP, stream->remoteAddr, dev);
+}
+
+void connected(struct netdevice *dev,
+        uint16_t localPort, uint16_t remotePort,
+        uint32_t remoteAddr, uint32_t ackSeq) {
+    uint32_t localSeq = 100;
+
+    sbuff * sb = ip_sbuff_alloc(sizeof(tcp_hdr));
+    tcp_hdr * hdr = (tcp_hdr*) sb->head;
+
+    hdr->srcPort = ntos(localPort);
+    hdr->destPort = ntos(remotePort);
+    hdr->sequence = ntol(localSeq);
+    hdr->ack = ntol(ackSeq + 1);
+    hdr->offset = 5;
+    hdr->reserved = 0;
+    hdr->flags = Syn | Ack;
+    hdr->window = ntos(65536 / 2);
+    hdr->chksum = 0;
+
+    tcp_checksum(sb, sizeof(*hdr), dev->ip, remoteAddr);
+
+    ip_send(sb, IPPROTO_TCP, remoteAddr, dev);
 }
 
 void tcp_segment(struct netdevice *dev, const uint8_t* data, uint32_t srcIp) {
@@ -55,6 +99,8 @@ void tcp_segment(struct netdevice *dev, const uint8_t* data, uint32_t srcIp) {
             stream s = {dst, ntos(hdr->srcPort), srcIp, 0, ntol(hdr->sequence), 1 };
             reset_stream(dev, &s);
         }
+        else {
+            connected(dev, dst, ntos(hdr->srcPort), srcIp, ntol(hdr->sequence) );
+        }
     }
-
 }
