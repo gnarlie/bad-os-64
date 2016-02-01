a6a679c13a5d79acdac54feeed7f08a5b238425d
Brian Adams <badams@gnarlie.com>
2016-02-01 08:44:35 -0600

    serial out

---
+++ src/serial.c
@@ -0,0 +1,13 @@
+#include "serial.h"
+#include "common.h"
+
+static int is_transmit_empty(int port) {
+    return inb(port + 5) & 0x20;
+}
+
+void serial_put(int com, char c) {
+    while (!is_transmit_empty(com)) ;
+
+    outb(com, c);
+}
+
---
+++ src/serial.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define COM1 0x3f8
+
+void serial_put(int basePort, char c);
